7c5a4b91b450920d6348d29db8eb3dcb56b06634
Elragency <elragency@gmail.com>
2023-08-05 13:33:14 -0400

    adding netlify2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: '@import "./src/styles/_mixins.scss";'
-        },
-      },
-    }
-  }
+  output: 'server',
+  adapter: netlify(),
 });
